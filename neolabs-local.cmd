63f1b5cea512523c3e43e7596a6a7fc794c7bdb7
khristos24 <166956265+khristos24@users.noreply.github.com>
2026-08-14 23:05:33 +0100

    Remove legacy Windows CLI wrapper

--- neolabs-local.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set "NEOLABS_LAB_BASE_URL=https://pg1wb0sklb.execute-api.us-east-1.amazonaws.com"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0neolabs.ps1" %*
-exit /b %ERRORLEVEL%
